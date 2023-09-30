fd31468979bcb92df48b593ae1559514052c2102
Guilherme Gomes <gguife747@gmail.com>
2023-09-30 12:22:58 -0300

    feat: full home component base

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,16 +1,37 @@
-import { HomePage } from './style';
-
-import { Header } from '../header/Header';
+import { HomeBottomContainer, HomePage, HomePrincipalContent } from './style';
+//Icon && Image
+import Logo from '../../assets/logo.png';
+import {FiGithub, FiLinkedin, FiInstagram} from 'react-icons/fi';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 
 
 export const Home = () => {
   return (
-    <>
-      <Header />
-      <HomePage>
-        Home
-      </HomePage>
-    </>
+    <HomePage>
+      <HomePrincipalContent>
+        <div className="home-desc">
+          <span>Olá, me chamo <br /> <strong>Guilherme Gomes.</strong></span>
+          <p>Sou <strong>desenvolvedor web</strong>, com habilidades nas áreas <br /> <strong>Front-end</strong> e <strong>Back-end</strong></p>
+          <img src={Logo} alt="Logo" width={300} />
+        </div>
+      </HomePrincipalContent>
+      <HomeBottomContainer>
+        <ul>
+          <li><a href="https://www.instagram.com/gguife/" target='__blank'><FiInstagram /></a></li>
+          <li><a href="https://github.com/Gguife" target='__blank'><FiGithub /></a></li>
+          <li><a href="https://www.linkedin.com/in/guilherme-gomes-b09444254/" target='__blank'><FiLinkedin /></a></li>
+        </ul>
+        <div className="fale-comigo">
+          <a href='#contact'>Fale <br /> Comigo <hr /></a>
+          <Player
+            src='https://lottie.host/cae1dc39-3c4c-44ca-a0e7-a491a3672fe4/13yVWciwVB.json'
+            className="talkToMe"
+            loop
+            autoplay
+          />
+        </div>
+      </HomeBottomContainer>
+    </HomePage>
   )
 }
--- src/components/home/style.ts
+++ src/components/home/style.ts
@@ -1,3 +1,89 @@
 import styled from "styled-components";
 
-export const HomePage = styled.section``;
+export const HomePage = styled.section`
+height: calc(100vh - 8rem);
+position: relative;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`;
+
+export const HomePrincipalContent = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+.home-desc{
+  position: relative;
+  background-color: #053B60;
+  color: #FFF;
+  padding: 20px 30px;
+  padding-right: 150px;
+  border-radius: 5px;
+  span{
+    font-size: 2rem;
+  }
+  img{
+    position: absolute;
+    top: -60px;
+    right: -110px;
+  }
+}
+`;
+
+export const HomeBottomContainer = styled.div`
+position: absolute;
+bottom: 0;
+width: 100%;
+display: flex;
+align-items: flex-end;
+justify-content: space-between;
+
+ul{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  li{
+    font-size: 2rem;
+    padding: 10px;
+    a{
+      color: ${({theme}) => theme.text};
+      transition: .3s;
+
+      &:hover{
+        color: purple;
+      }
+    }
+  }
+}
+
+.fale-comigo{
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  gap: 10px;
+  a{
+    font-weight:700;
+    text-decoration: none;
+    color: ${({theme}) => theme.text};
+    font-size: 1.3rem;
+    text-align: end;
+    line-height: 20px;
+    transition: .3s;
+
+    hr{
+      width: 50%;
+      height: 1px;
+      border: 0px;
+      border-top: 3px solid purple;
+    }
+    &:hover{
+      color: purple;
+    }
+  }
+  .talkToMe{
+    width: 50px;
+  }
+}
+`;
