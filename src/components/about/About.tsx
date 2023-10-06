a710fe6580c2cd9f17aa3056bd2fad28ca5fdcb6
Gguife <gguife.747@gmail.com>
2023-10-06 18:23:26 -0300

    feat: create section About and your resposinve

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,13 +1,60 @@
-import { AboutContainer, AboutContentDesc, AboutContentImg, AboutSection } from "./style"
+import { AboutContainer, AboutContentDesc, AboutContentImg, AboutSection, TabButton, TabContent} from "./style"
 import { Player } from '@lottiefiles/react-lottie-player';
+import { useState } from "react";
+
+const TAB_DATA = [
+    {
+      title: "Skills",
+      id: "Skills",
+      content: (
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>Nodejs</li>
+            <li>SQL(MySQL) e NoSQL(MongoDB)</li>
+        </ul>
+      ),
+    },
+    {
+        title: "Acadêmico",
+        id: "Academico",
+        content: (
+        <ul>
+            <li>Engenharia de Software - Estácio</li>
+            <li>Formação em Tecnologia - Escola DNC</li>
+        </ul>
+        ),
+    },
+    {
+      title: "Certificado",
+      id: "Certificado",
+      content: (
+        <ul>
+            <li>Bootcamp Java back-end - Santander Dio</li>
+            <li>Web Responsive - FreeCodeCamp</li>
+            <li>Tecnologia da Informação - SENAI</li>
+        </ul>
+      ),
+    },
+  ];
+
 
 export const About = () => {
+const [activeTab, setActiveTab] = useState("Skills");
+
+const handleTabClick = (title: string) => {
+    setActiveTab(title);
+};
+
     return (
         <AboutSection id="sobre">
             <AboutContainer>
                 <AboutContentImg>
                 <Player
-                    src='https://lottie.host/4298b2e0-018f-4199-8d9a-60761cfb0548/CjFBW9qb05.json'
+                    src='https://lottie.host/7744b807-840a-4091-a435-71b21e843bb4/81EH49udnj.json'
                     className="software"
                     loop
                     autoplay
@@ -19,34 +66,15 @@ export const About = () => {
                         <p>Sou um desenvolvedor full stack apaixonado por criar aplicações web interativas e responsivas. Possuo uma sólida experiência na utilização de tecnologias como JavaScript, React, Redux, Node.js, Express, MongoDB, SQL, HTML, CSS e Git. Estou constantemente em busca de expandir meu conjunto de conhecimentos e competências. Além disso, sou altamente colaborativo e entusiasta em relação ao trabalho em equipe, ansioso para colaborar com outros profissionais na criação de aplicativos excepcionais.</p>
                     </div>
                     <div className="desc-estudos">
-                        <div className="skills">
-                            <button>Skills</button>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>React</li>
-                                <li>TypeScript</li>
-                                <li>Nodejs</li>
-                                <li>MongoDB</li>
-                                <li>MySQL</li>
-                            </ul>
+                    {TAB_DATA.map((tab) =>(
+                        <div key={tab.title}>
+                            <TabButton onClick={() => handleTabClick(tab.title)} active={activeTab === tab.title}>
+                                {tab.title}
+                                <div className="border-bottom"></div>
+                            </TabButton>
+                            <TabContent active={activeTab === tab.title} className={tab.title}>{tab.content}</TabContent>
                         </div>
-                        <div className="academico">
-                            <button>Acadêmico</button>
-                            <ul>
-                                <li>Engenharia de Software - Estácio</li>
-                                <li>Formação em Tecnologia - Escola DNC</li>
-                            </ul>
-                        </div>
-                        <div className="certificados">
-                            <button>Certificados</button>
-                            <ul>
-                                <li>Bootcamp Java back-end - Santander Dio</li>
-                                <li>Web Responsive - FreeCodeCamp</li>
-                                <li>Tecnologia da Informação - SENAI</li>
-                            </ul>
-                        </div>
+                    ))}
                     </div>
                 </AboutContentDesc>
             </AboutContainer>
--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -1,17 +1,39 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
+//Type
+interface TabContentProps {
+  active: boolean;
+}
+interface TabButtonProps {
+    active: boolean;
+}
+
+//Section
 export const AboutSection = styled.section`
-    
+background-color: #333;
+color: #FFF;
+
+@media(max-width: 1000px){
+    height: 50rem;
+}
 `;
 
+//Principal Content
 export const AboutContainer = styled.div`
 display: flex;
-align-items: center;
-justify-content: center;
+align-items: start;
+justify-content: start;
 max-width: 1200px;
 margin: 0 auto;
 padding: 20px;
+gap: 3rem;
 
+@media(max-width: 1000px){
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  
+    gap: 0rem;
+}
 `;
 
 export const AboutContentImg = styled.div`
@@ -20,39 +42,97 @@ width: 100%;
     width: 100%;
     border-radius: 10px;
 }
+
+@media(max-width: 1000px){
+    width: 50%;
+}
+@media(max-width: 500px){
+    width: 70%;
+}
 `;
 
 export const AboutContentDesc = styled.div`
 width: 100%;
+position: relative;
+padding-top: 3rem;
 
 .desc-sobremim{
     padding: 10px;
+    p{
+        font-size: .9rem;
+    }
 }
 
 .desc-estudos{
     display: flex;
     gap: 1rem;
     padding: 10px;
-    position: relative;
+}
 
-    .skills, .academico, .certificados{
-        button{
-            border: none;
-            border-bottom: 5px solid purple;
-            font-size: 1rem;
-            font-weight: 700;
-            padding: 5px 5px;
-            background-color: transparent;
-            color: #999;
-            cursor: pointer;
-            
-            &:hover{
-                color: ${({theme}) => theme.text};
-            }
-        }
+@media(max-width: 1000px){
+    padding-top: 0rem;
+}
+
+@media(max-width: 500px){
+    h1{
+        font-size: 1.3rem;
     }
-    ul{
-        display: none;
+    p{
+        font-size: .6rem;
     }
 }
+`;
+
+
+
+
+//List
+export const TabContent = styled.ul<TabContentProps>`
+padding: 10px;
+display: ${(props) => (props.active ? "block" : "none")};
+position: absolute;
+left: 2rem;
+    
+    li{
+        list-style-type: disc;
+        font-weight: 700;
+        font-size: .85rem;
+    }
+
+@media(max-width: 500px){
+    li{
+        font-size: .7rem;
+    }
+}
+`;
+
+const horizontalAnimation = keyframes`
+0% {
+    width: 0;
+}
+100% {
+    width: 100%;
+}
+`;
+
+export const TabButton = styled.button<TabButtonProps>`
+border: none;
+font-size: 1rem;
+font-weight: 700;
+padding: 5px 5px;
+background-color: transparent;
+color: #999;
+cursor: pointer;
+
+${(props) =>
+    props.active &&
+    css`
+    color: #fff;
+    .border-bottom{
+        background-color: purple;
+        width: 100%;
+        height: .2rem;
+        animation: ${horizontalAnimation} 0.3s linear forwards;
+    }
+    `}
 `;
