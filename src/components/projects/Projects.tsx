043e56371f231a218eae11584cc4dbea0a31fe1f
Gguife <gguife.747@gmail.com>
2023-10-08 14:36:37 -0300

    feat: create Project section and your reposinve page

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { ProjectSection, ProjectButton, ProjectCard } from "./style";
+//MOCK
+import { PROJECTS_MOCK } from "../../mock/projectCard";
+//ICONS
+import { TbWorldCode, TbCode } from "react-icons/tb";
+
+
+export const Projects = () =>{
+  const [activeTab, setActiveTab] = useState("Front-end");
+
+  const handleTabClick = (category: string) => {
+    setActiveTab(category);
+  };
+
+  // Filtrar projetos com base na categoria ativa
+  const filteredProjects = PROJECTS_MOCK.filter(
+    (project) => project.category === activeTab
+  );
+  return (
+    <ProjectSection>
+      <h1>Meus Projetos</h1>
+      <ProjectButton>
+        <button onClick={() => handleTabClick("Front-end")} className={activeTab === "Front-end" ? "active-button" : ""}>Front-end</button>
+        <button onClick={() => handleTabClick("Back-end")} className={activeTab === "Back-end" ? "active-button" : ""}>Back-end</button>
+        <button onClick={() => handleTabClick("Clientes")} className={activeTab === "Clientes" ? "active-button" : ""}>Clientes</button>
+      </ProjectButton>
+      <ProjectCard>
+      {filteredProjects.map((project) => (
+          <div className="project-card" key={project.id}>
+            <img src={project.imgPath} alt="Imagem do Projeto" />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <a href={project.github} target="_blank" rel="noopener noreferrer">
+              <TbCode />
+            </a>
+            <a href={project.web} target="_blank" rel="noopener noreferrer">
+              <TbWorldCode />
+            </a>
+          </div>
+        ))}
+      </ProjectCard>
+    </ProjectSection>
+  )
+}
---
+++ src/components/projects/style.ts
@@ -0,0 +1,120 @@
+import styled from "styled-components";
+
+export const ProjectSection = styled.section`
+max-width: 1200px;
+margin: 0 auto;
+padding: 2rem 0;
+height: 55vw;
+
+h1{
+  text-align: center;
+}
+`;
+
+export const ProjectButton = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 2rem 0;
+gap: 3rem;
+
+button{
+  padding: 1rem 1.5rem;
+  background-color: ${({theme}) => theme.body};
+  border: 2px solid purple;
+  border-radius: 5px;
+  color: ${({theme}) => theme.text};
+  font-weight: 700;
+  cursor: pointer;
+  transition: .4s;
+  
+}
+
+.active-button{
+  background-color: purple;
+  color: #FFF;
+}
+
+
+@media(max-width: 500px){
+  gap: 2rem;
+  
+  button{
+    padding: .8rem 1rem; 
+  }
+}
+`;
+
+export const ProjectCard = styled.div`
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+place-items: start;
+padding: 10px;
+gap: 3rem;
+
+.project-card{
+  width: 100%;
+  background-color: #999;
+  padding: 1rem;
+  border-radius: 10px;
+  
+  img{
+    width: 100%;
+    border-radius: 3px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  }
+  h3{
+    color: purple;
+  }
+  p{
+
+  }
+  a{
+    color: #000;
+    font-size: 1.2rem;
+    padding-right: 1rem;
+    
+    &:hover{
+      color: purple;
+    }
+  }
+}
+
+@media(max-width: 900px){
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+}
+
+@media(max-width: 500px){
+  grid-template-columns: repeat(1, 1fr);
+  gap: 2rem;
+  place-items: center;
+  justify-content: center;
+  
+  .project-card{
+    width: 80%;
+    
+    img{
+      width: 100%;
+      border-radius: 3px;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+    }
+    h3{
+      color: purple;
+    }
+    p{
+  
+    }
+    a{
+      color: #000;
+      font-size: 1.2rem;
+      padding-right: 1rem;
+      
+      &:hover{
+        color: purple;
+      }
+    }
+  }
+  
+}
+`;
