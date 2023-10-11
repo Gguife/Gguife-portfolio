e31d34107cb5115851fafe00198754eb865ee1df
Gguife <gguife.747@gmail.com>
2023-10-11 14:17:22 -0300

    style: responsive Lang component

--- src/components/lang/style.ts
+++ src/components/lang/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const LangContainer = styled.div`
 background-color: purple;
 color: #FFF;
-margin: 1rem 0;
+margin: 3rem 0;
 padding: .5rem; 
 `;
 
@@ -11,11 +11,14 @@ export const LangsCard = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
-font-size: .5rem;
+font-size: 1rem;
 gap: 1rem;
 
-
 .lang-icon{
+  padding: .3rem 0;
+}
 
+@media (max-width: 600px){
+  font-size: .5rem; 
 }
 `;
