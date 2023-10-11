31073d99ef5a6bba39171b1bfaee7199696feced
Gguife <gguife.747@gmail.com>
2023-10-10 21:38:44 -0300

    feat: create scroll up button

---
+++ src/components/scrollUp/ScrollUp.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import './scrollup.css';
+//ICON
+import{ BiSolidUpArrowSquare } from 'react-icons/bi';
+
+export const ScrollUp = () =>{
+  
+  useEffect(() => {
+    window.addEventListener("scroll", function() {
+      const scrollUp = document.querySelector(".scrollup");
+      if (scrollUp) {
+        if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+        else scrollUp.classList.remove("show-scroll");
+      }
+    });
+  }, [])
+
+  return (
+    <a href='#home' className='scrollup'>
+      <BiSolidUpArrowSquare className='scrollup-icon' />
+    </a>
+    )
+}
