9f8f467f61dfce5cd9ae5ab47a097357082e0b42
Gguife <gguife747@gmail.com>
2024-03-18 11:02:09 -0300

    feat: New header layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,26 @@
-import { HeaderContainer, HeaderMenu } from "./style";
+import { HeaderContainer, HeaderMenu, HeaderButton } from "./style";
+
+interface HeaderProps {
+  ThemeButton: any;
+}
 
 
-
-export const Header = () => {
+export const Header = ({ThemeButton}: HeaderProps) => {
 
   return (
     <HeaderContainer id="home">
-      <div className="logo">
-        <span>GGUIFE</span>
-      </div>
       <HeaderMenu>
+        <span>Gguife</span>
         <ul>
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#projetos">Projetos</a></li>
           <li><a href="#contato">Contato</a></li>
         </ul>
       </HeaderMenu>
+      <HeaderButton>
+        <a href="#contato">Fale comigo</a>
+        {ThemeButton}
+      </HeaderButton>
     </HeaderContainer>
   )
 }
