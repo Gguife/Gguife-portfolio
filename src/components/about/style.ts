bbd2033da4586d134b96ba6c9132ed30edda22e0
Gguife <gguife747@gmail.com>
2024-03-18 22:20:29 -0300

    feat: Added new layout style in section about me

--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -12,73 +12,101 @@ interface TabButtonProps {
 export const AboutSection = styled.section`
 background-color: #333;
 color: #FFF;
-
-@media(max-width: 1000px){
-  height: 57rem;
-}
-@media(max-width: 800px){
-  height: 50rem;
-}
 `;
 
 //Principal Content
 export const AboutContainer = styled.div`
 display: flex;
-align-items: start;
-justify-content: start;
+justify-content: space-between;
 max-width: 1200px;
+height: 600px;
 margin: 0 auto;
 padding: 20px;
 gap: 3rem;
 
-@media(max-width: 1000px){
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;  
-    gap: 0rem;
+@media(max-width: 900px){
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;  
+  gap: 2rem;
+  height: 900px;
 }
 `;
 
 export const AboutContentImg = styled.div`
-width: 100%;
-.software{
-    width: 100%;
-    border-radius: 10px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding: 20px;
+gap: 2rem;
+height: 100%;
+
+img{
+  max-width: 1000px;
+  width: 100%;
 }
 
-@media(max-width: 1000px){
-    width: 50%;
+.button-cv{
+  background-color: #222;
+  border: none;
+  padding: .7rem 2rem;
+  border-radius: 5px;
+  font-weight: 700;
+  color: #4AAADA;
+  cursor: pointer;
+  box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
 }
+
+@media(max-width: 900px){
+  margin-top: 4rem;
+  img{
+    max-width: 400px;
+    width: 100%;
+  }
+}
+
 @media(max-width: 500px){
-    width: 70%;
+  margin-top: 5rem;
+  img{
+    max-width: 400px;
+    width: 100%;
+  }
+  .button-cv{
+    padding: .5rem 1.5rem;
+    font-size: .7rem;
+  }
 }
+
 `;
 
 export const AboutContentDesc = styled.div`
 width: 100%;
 position: relative;
-padding-top: 1rem;
-
-.desc-sobremim{
-  padding: 10px;
-    
+padding: 10px;
+max-width: 550px;
+  
+  h1{
+    font-size: 2.5rem;
+    padding: 0 10px;
+  }
+  
+  .desc-sobremim{
+    padding: 10px;
     p{
-        font-size: .9rem;
-        margin: 5px 0;
+      font-size: .8rem;
+      margin-bottom: .5rem;
     }
-}
-
-.desc-estudos{
+  }
+  
+  .desc-estudos{
     display: flex;
     gap: 1rem;
-    padding: 10px;
-}
-
-@media(max-width: 1000px){
-  padding-top: 0rem;
-
-  .desc-sobremim{
-
+  }
+  
+  @media(max-width: 1000px){
+    padding-top: 0rem;    
+    .desc-sobremim{
     p{
       font-size: .8rem;
     }
@@ -104,18 +132,23 @@ export const TabContent = styled.ul<TabContentProps>`
 padding: 10px;
 display: ${(props) => (props.active ? "block" : "none")};
 position: absolute;
-left: 2rem;
-    
+left: 1rem;
+  ul{
+    list-style: none;
+    z-index: 999;
     li{
-        list-style-type: disc;
-        font-weight: 700;
-        font-size: .85rem;
+      font-weight: 700;
+      font-size: .85rem;
     }
+  }
+
 
 @media(max-width: 500px){
+  ul{
     li{
-        font-size: .7rem;
+      font-size: .7rem;
     }
+  }
 }
 `;
 
@@ -140,9 +173,10 @@ cursor: pointer;
 ${(props) =>
     props.active &&
     css`
-    color: #fff;
+    color: #4AAADA;
     .border-bottom{
-        background-color: purple;
+        background-color: #4AAADA;
+        border-radius: 2px;
         width: 100%;
         height: .2rem;
         animation: ${horizontalAnimation} 0.3s linear forwards;
