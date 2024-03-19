10ac014a35b0c3857063bcba84a4970aadcbeeb1
Gguife <gguife747@gmail.com>
2024-03-18 22:40:45 -0300

    style: Correct primary color title

--- src/components/projects/style.ts
+++ src/components/projects/style.ts
@@ -91,27 +91,6 @@ gap: 3rem;
   
   .project-card{
     width: 80%;
-    
-    img{
-      width: 100%;
-      border-radius: 3px;
-      box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-    }
-    h3{
-      color: purple;
-    }
-    p{
-  
-    }
-    a{
-      color: #000;
-      font-size: 1.2rem;
-      padding-right: 1rem;
-      
-      &:hover{
-        color: purple;
-      }
-    }
   }
   
 }
