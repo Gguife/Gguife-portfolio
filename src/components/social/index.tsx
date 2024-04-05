3c85fe18baa884988b00288394833e2c59f2fef5
Gguife <gguife747@gmail.com>
2024-04-05 12:09:50 -0300

    feat: Added correct social links

--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -5,9 +5,9 @@ import {FaTiktok } from 'react-icons/fa';
 export const Social = () =>{
   return(
     <SocialMedia>
-      <a href="https://www.instagram.com/gguife/" target='__blank'><FaTiktok /></a>
-      <a href="https://www.instagram.com/gguife/" target='__blank'><FiTwitter /></a>
-      <a href="https://www.instagram.com/gguife/" target='__blank'><FiInstagram /></a>
+      <a href="https://www.tiktok.com/@gguifedev" target='__blank'><FaTiktok /></a>
+      <a href="https://twitter.com/gguifedev127384" target='__blank'><FiTwitter /></a>
+      <a href="https://www.instagram.com/gguifedev/" target='__blank'><FiInstagram /></a>
       <a href="https://github.com/Gguife" target='__blank'><FiGithub /></a>
       <a href="https://www.linkedin.com/in/gguife/" target='__blank'><FiLinkedin /></a>
       <div className="hr-social"></div>
