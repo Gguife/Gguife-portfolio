de51fd9b980707bc30f8c5bf2fb6417e0382a6de
Gguife <gguife747@gmail.com>
2024-11-23 01:06:15 -0300

    feat: Added new endpoints

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -2,14 +2,24 @@ import { useRoutes } from "react-router-dom";
 import About from "../views/about/index.tsx";
 import Main from "../views/main/index.tsx";
 import Login from "../views/login/index.tsx";
-import Posts from "../views/posts/index.tsx";
+import ManagePost from "../views/manage/index.tsx";
+import ProtectedRoute from "../components/jwt/index.tsx";
+import NotFound from "../views/404/index.tsx";
 
 export const MainRouter = () =>{
   const router = useRoutes([
     {path: "/", element: <Main />},
     {path: "/sobre", element: <About />},
     {path: "/login", element: <Login />},
-    {path: "/all-posts", element: <Posts />},
+    {
+      path: "/gerencia",
+      element: (
+        <ProtectedRoute>
+          <ManagePost />
+        </ProtectedRoute>
+      )
+    },
+    { path: "*", element: <NotFound /> },
   ])
 
   return router;
