7f0f8670a73f16fe86710a9473d56310960a18b8
Gguife <gguife747@gmail.com>
2024-11-29 15:10:46 -0300

    feat: Adding token authentication for pages permission

--- src/components/jwt/index.tsx
+++ src/components/jwt/index.tsx
@@ -1,14 +1,29 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+interface JwtPayload {
+  exp: number;
+}
 
 const isAuthenticated = () => {
   const token = localStorage.getItem("token");
 
-  if(token){
-    return true;
-  }
+  if(!token) return false;
 
-  return false;
+  try {
+    const {exp} = jwtDecode<JwtPayload>(token);
+
+    if(Date.now() >= exp * 1000){
+      localStorage.removeItem("token");
+      return false;
+    }
+
+
+    return true;
+  } catch(err: any){
+    console.error("Erro na autenticação: ", err);
+  }
 }
 
 interface ProtectedRouteProps {
