faea381864e52e8f00a7d8bf126650fd058f0766
Gguife <gguife747@gmail.com>
2024-12-06 16:04:02 -0300

    refactor:  Move files to a new folder structure

---
+++ src/views/dashboard/project/create/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CreateProjectSection = styled.section`
+padding: 3rem 2rem;
+display: flex;
+align-items: center;
+justify-content: center;
+
+form{
+  select{
+    padding: .7rem;
+    border: none;
+    border-radius: 4px;
+  }
+}
+`;
