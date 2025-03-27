7132afd961ca05e28926ab1f03e368e4d643fd82
Gguife <gguife747@gmail.com>
2025-03-27 04:00:04 -0300

    refactor

--- src/views/login/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const LoginSection = styled.div`
-padding: 2rem;
-height: 70dvh;
-display: flex;
-align-items: center;
-justify-content: center;
-`;
