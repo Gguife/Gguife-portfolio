36e21581bc4a7506e74f37f3c783e3b728128125
Gguife <gguife747@gmail.com>
2025-04-16 00:17:14 -0300

    feat: add blog and projects routes

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -2,13 +2,19 @@ import { useRoutes } from "react-router-dom";
 import About from "../views/about/index.tsx";
 import Main from "../views/main/index.tsx";
 import NotFound from "../views/404/index.tsx";
-import ProjectSelected from "../components/projects/projectId/Project.tsx";
+import ProjectsPage from "../views/project/allProjects/index.tsx";
+import ProjectSelected from "../views/project/projectID/index.tsx";
+import ArticlePage from "../views/article/blog/index.tsx";
+import ArticleSelected from "../views/article/articleID/index.tsx";
 
 export const MainRouter = () =>{
   const router = useRoutes([
     {path: "/", element: <Main />},
     {path: "/sobre", element: <About />},
+    {path: "/projetos", element: <ProjectsPage />},
     {path: "/projeto/:id", element: <ProjectSelected />},
+    {path: "/blog", element: <ArticlePage />},
+    {path: "/artigo/:id", element: <ArticleSelected />},
     { path: "*", element: <NotFound /> },
   ])
 
