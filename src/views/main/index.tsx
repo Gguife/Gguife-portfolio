cedc21e060a6880ffcfab75d4f07814ccadf84e5
Gguife <gguife747@gmail.com>
2025-05-10 23:58:45 -0300

    feat: add article and projects components and finished main page

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,126 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { MainPageSection, MainAboutMeCard, MainArticleSection, ArticleCard } from "./style";
+import { MainPageSection, MainAboutMeCard, ArticlesSection, ProjectsSection } from "./style";
 
-interface Article {
-  id: number;
-  title: string;
-  introduction: string;
-  tagId: number;
-  createdAt: string;
-}
+//icons
+import { FaHtml5, FaReact, FaNodeJs, FaGitAlt, FaCss3Alt, FaDocker, FaLinux, FaJava, FaWindows  } from 'react-icons/fa';
+import { SiMysql, SiPostgresql, SiSpringboot} from 'react-icons/si';
+import { TbBrandTypescript } from 'react-icons/tb';
+import { IoLogoJavascript } from "react-icons/io";
+import { Projects } from "../../components/projects";
+import { Articles } from "../../components/articles";
 
-const tagNames: { [key: number]: string } = {
-  1: "Tech",
-  2: "Lifestyle",
-  3: "Career"
-}
+
+const TAB_DATA = [
+  {
+    title: "tecnologias",
+    id: "tecnologias",
+    content: (
+      <ul>
+      <li><FaHtml5 className="icons"/></li>
+      <li><FaCss3Alt className="icons"/></li>
+      <li><FaGitAlt className="icons"/></li>
+      <li><FaLinux  className="icons"/></li>
+      <li><SiMysql className="icons"/></li>
+      <li><FaNodeJs className="icons"/></li>
+      <li><FaReact className="icons"/></li>
+      <li><FaDocker className="icons"/></li>
+      <li><FaJava className="icons"/></li>
+      <li><SiSpringboot className="icons"/></li>
+      <li><FaWindows  className="icons"/></li>
+      <li><IoLogoJavascript className="icons"/></li>
+      <li><TbBrandTypescript className="icons"/></li>
+      <li><SiPostgresql className="icons"/></li>
+    </ul>
+    ),
+  }
+];
 
 const MainPage: React.FC = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [filteredArticles, setFilteredArticles] = useState<Article[]>([]);
-  const [selectedTag, setSelectedTag] = useState(1);
-  const limit = 9;
-
-  
-  useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        const username = 'linux';
-        const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/articles/${username}?limi=${limit}`
-        );
-
-        console.log(response.data.articles)
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.error("Erro ao buscar artigos", error);
-      }
-    };
-    
-    fetchArticles();
-  }, []); 
-
-  useEffect(() => {
-      filteredArticleByTag(selectedTag);
-  }, [articles, selectedTag]);
-
-  const filteredArticleByTag = (tagId: number) => {
-    const filtered = articles.filter(
-      (article) => article.tagId === tagId
-    );
-    setFilteredArticles(filtered);
-    console.log(filtered);
-  }
 
   return (
-
-    <MainPageSection>
-      <MainAboutMeCard>
-        <div className="main-about-description">
-          <h3>Guilherme Gomes<span>Desenvolvedor web full-stack</span></h3>
-          <p>
-            Criando soluções que conectam o que está por trás das telas com o que vemos.
-            Aqui, compartilho experiências, desafios e conquistas sobre tecnologia, programação, cibersegurança e minha jornada pessoal, 
-            com objetivo de deixar a tecnologia mais simples e acessível, 
-            enquanto navego pelo mundo digital.
-          </p>
-          <Link to="/sobre" className="internal-links">Mais sobre mim ➔</Link>
-        </div>
-      </MainAboutMeCard>
-
-      <MainArticleSection>
-        <h1>Blog Pessoal</h1>
-
-        <ArticleCard>
-          {filteredArticles.length === 0 ? (
-            <p className="no-articles">Sem postagens para esta categoria...</p>
-          ) : (
-            <div className="articles-list">
-            {filteredArticles.map((article) => (
-              <div className="article-card">
-                <span>{article.createdAt}<span className="tag">{tagNames[article.tagId]}</span></span>
-                <h3> 
-                  {article.title} 
-                  <Link to={`/artigo/${article.id}`} className="internal-links read-about" key={article.id}>
-                  Leia sobre ➔
-                  </Link>
-                </h3> 
-                <p>{article.introduction}</p> 
+    <>
+      <MainPageSection>
+        <MainAboutMeCard>
+          <div className="main-about-description">
+            <h3>Guilherme Gomes<span>Desenvolvedor web full-stack</span></h3>
+            <p>
+              Criando soluções que conectam o que está por trás das telas com o que vemos.
+              Aqui, compartilho experiências, desafios e conquistas sobre tecnologia, programação, cibersegurança e minha jornada pessoal, 
+              com objetivo de deixar a tecnologia mais simples e acessível, 
+              enquanto navego pelo mundo digital.
+            </p>
+            {TAB_DATA.map((tab) =>(
+              <div className="techs-card" key={tab.title}>
+                  <div className="tech">{tab.content}</div>
               </div>
             ))}
-            
           </div>
-          )}
+        </MainAboutMeCard>
+      </MainPageSection>
+      <ArticlesSection>
+        <Articles />
+      </ArticlesSection>
 
-          <div className="article-tags">
-            <button onClick={() => setSelectedTag(1)}
-              style={{
-                backgroundColor: selectedTag === 1 ? "var(--text-link-subcolor)" : "var(--primary-soft-color)",
-                color: selectedTag === 1 ? "var(--text-primary-color)" : "var(--text-second-color)",
-            }}>
-              Tech
-            </button>
-            <button onClick={() => setSelectedTag(2)}
-              style={{
-                backgroundColor: selectedTag === 2 ? "var(--text-link-subcolor)" : "var(--primary-soft-color)",
-                color: selectedTag === 2 ? "var(--text-primary-color)" : "var(--text-second-color)",
-            }}>
-              lifestyle
-            </button>
-            <button onClick={() => setSelectedTag(3)}
-              style={{
-                backgroundColor: selectedTag === 3 ? "var(--text-link-subcolor)" : "var(--primary-soft-color)",
-                color: selectedTag === 3 ? "var(--text-primary-color)" : "var(--text-second-color)",
-            }}>
-              Career
-            </button>
-          </div>
-        </ArticleCard>
-        <Link to="/blog" className="internal-links read-more">Veja todos os artigos ➔</Link>
-      </MainArticleSection>
-    </MainPageSection>
+      <ProjectsSection>
+        <Projects />
+      </ProjectsSection>
+    </>
   );
 };
 
--- src/views/main/style.ts
+++ src/views/main/style.ts
@@ -13,9 +13,6 @@ padding: 2rem 1rem;
     font-size: var(--text-small-size);
   }
 }
-
-@media(max-width: 500px){
-}
 `;
 
 export const MainAboutMeCard = styled.div`
@@ -49,6 +46,27 @@ margin: 2rem 0;
     font-size: var(--text-medium-size);
     margin-bottom: 2rem;
   }
+
+
+  .techs-card {
+    display: flex;
+    align-items:center;
+    justify-content: center;
+
+    .tech {
+      ul {
+        display: flex;
+        list-style: none;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+
+        li{ 
+          font-size: 1.1rem;
+        }
+      }
+    }
+  }
 }
 
 
@@ -80,8 +98,25 @@ margin: 2rem 0;
       text-align: center;
       margin-bottom: .7rem;
     }
+
+
+    .techs-card {
+      .tech {
+        ul {
+          gap: .7rem;
+          li{ 
+            font-size: .8rem;
+          }
+        }
+      }
+    }
   }
+
+
+
 }
+
+
 @media(max-width: 500px){
   padding: 1rem 2rem;
   margin-bottom: 0;
@@ -89,198 +124,26 @@ margin: 2rem 0;
   img{
     max-width: 100px;
   }
+
+  .main-about-description {
+    .techs-card {
+      .tech {
+        ul {
+          li{ 
+            font-size: .6rem;
+          }
+        }
+      }
+    }
+  }
 }
 `;
 
-export const MainArticleSection = styled.section`
-margin-top: 2rem;
-padding: 1rem;
-margin-bottom: 2rem;
-
-  h1{
-    font-size: var(--text-title-size);
-    text-align: start;
-    margin-bottom: 1rem;
-  }
-
-
-@media(max-width: 800px){
-  margin-top: 1rem;
-  padding: 1rem;
-  
-  h1 {
-    font-size: var(--text-subtitle-size);
-    text-align: center;
-  }
-
-  .read-more {
-    font-size: .7rem;
-    padding-left: 2rem;
-  }
-}
-
-@media(max-width: 530px){
-  margin-bottom: 1rem;
-  h1 {
-    font-size: var(--text-medium-size);
-  }
-
-  .read-more {
-    font-size: .7rem;
-    padding-left: 1rem;
-  }
-}
-
+export const ArticlesSection = styled.div`
+  background-color: var(--primary-soft-color);
+  padding: 2rem 0;
 `;
 
-
-
-export const ArticleCard = styled.div`
-padding: 2rem 0;
-display: flex;
-justify-content: space-between;
-
-  h1{
-    font-size: var(--text-title-size);
-    padding: 3rem 0;
-    top: 0;  
-  }
-
-  .articles-list{
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      gap: var(--gap-small-size);
-      margin: 0 0 1rem .5rem;     
-          
-
-    .article-card{
-      max-width: 600px;
-
-      span {
-        font-size: var(--text-small-size);
-        
-        .tag {
-          margin-left: .3rem;
-          background-color: var(--text-second-color);
-          color: #000;
-          border-radius: 5px;
-          padding: .1rem 1rem;
-          font-wieght: 700;      
-        }
-      }
-
-      h3{
-        font-size: var(--text-medium-size);
-        color: var(--text-primary-color);
-        margin-top: .7rem;
-
-        .read-about {
-          padding-left: 1rem;
-        }
-      }
-      
-      p{
-        font-size: var(--text-medium-size);
-        color: var(--text-second-color);
-        line-height: .9rem;
-        padding: .4rem 0 .7rem 0;
-        max-width: 400px;
-      }
-    }
-  }
-
-  .article-tags {
-    display: flex;
-    flex-direction: column;
-    gap: var(--gap-small-size);    
-    
-    button {
-      border: none;
-      font-size: var(--text-medium-size);
-      padding: .2rem .4rem .2rem 1rem;
-      border-radius: 4px;
-      cursor: pointer;
-      transition: .4s;
-    }
-  }
-
-
-
-@media(max-width: 800px){
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem 0rem;
-
-  .no-articles{
-    margin-bottom: 2rem;
-    font-size: var(--text-small-size);
-  }
-
-  .articles-list{
-    grid-template-columns: repeat(2, 1fr);
-    margin: 0 0 1rem 0rem;
-
-    .article-card{
-      padding-left: 1rem;
-      
-      span {
-        font-size: .6rem;
-        padding: .2rem .7rem;
-        border-radius: 3px;
-      }
-
-      h3{
-        font-size: var(--text-small-size);
-
-        .read-about {
-          font-size: .7rem;
-          padding-left: .3rem;
-        }
-      }
-      
-      p{
-        font-size: var(--text-small-size);
-      }
-    }
-  }
-
-
-  .article-tags {
-    flex-direction: row;   
-    margin-bottom: 1rem;   
-
-    button {
-      font-size: .7rem;
-      padding: .2rem .7rem;
-    }
-  }
-}
-
-
-@media(max-width: 500px) {
-  padding: .5rem 0;
-  display: flex;
-  align-items: start;
-  flex-direction: row;
-  gap: 2rem;
-
-  .articles-list{
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    gap: .5rem;
-
-    .aritcle-card{
-      padding-left: 0rem;
-
-    }
-  }
-
-
-  .article-tags {   
-    margin-bottom: 0;   
-    flex-direction: column; 
-  }
-}
+export const ProjectsSection = styled.div`
+  padding: 2rem 0;
 `;
